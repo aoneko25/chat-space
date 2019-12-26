8cb94f4883090f610290d8ccc587169111e2d998
aoneko25 <aoneko820@yahoo.co.jp>
2019-12-26 13:37:43 +0900

    new
    hidouki

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
-$(function(){
+$(function(){ 
+    function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-info">
+            <div class="upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-info">
+            <div class="upper-info__talker">
+              ${message.user_name}
+            </div>
+            <div class="upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__text">
+            <p class="message__text__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -11,5 +51,15 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);  
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});    
+      $('form')[0].reset();
+      $('input').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+  });
   });
 });
