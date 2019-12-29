79bc4a96e483a365a71d056b423bf17571482933
aoneko25 <aoneko820@yahoo.co.jp>
2019-12-30 00:11:52 +0900

    zidou
    new

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,120 +1,109 @@
 $(function(){ 
   last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
   var buildHTML = function(message) {
     if (message.content && message.image) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-      //$(function(){});の閉じタグの直上(処理の最後)に以下のように追記
+      var html = `<div class="message" data-message-id= ${message.id}>
+        <div class="upper-info"> 
+          <div class="upper-info__talker"> 
+            ${message.user_name} 
+          </div> 
+          <div class="upper-info__date"> 
+            ${message.created_at} 
+          </div>
+        </div> 
+        <div class="message__text"> 
+          <p class="message.content"> 
+            ${message.content} 
+          </p> 
+          <img src= ${message.image} class="lower-message__image" > 
+        </div> 
+      </div>`
     } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="message" data-message-id= ${message.id}>
+        <div class="upper-info"> 
+          <div class="upper-info__talker"> 
+            ${message.user_name }
+          </div> 
+          <div class="upper-info__date"> 
+            ${message.created_at} 
+          </div> 
+        </div> 
+        <div class="message__text"> 
+          <p class="message.content"> 
+            ${message.content} 
+          </p> 
+        </div> 
+      </div>`
     } else if (message.image) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-      if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-        setInterval(reloadMessages, 7000);
-      }
+      var html = `<div class="message" data-message-id= ${message.id}>
+        <div class="upper-info">
+          <div class="upper-info__talker">
+            ${message.user_name}
+          </div>
+          <div class="upper-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message__text">
+          <img src= ${message.image} class="lower-message__image" >
+        </div> 
+      </div>`
     };
     return html;
   };
-  $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);  
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});    
-      $('form')[0].reset();
-    })
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    })
-    .always(function(){
-      $('input').prop('disabled', false);
-    });
+$('#new_message').on('submit', function(e){
+  e.preventDefault()
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);  
+    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});    
+    $('form')[0].reset();
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  })
+  .always(function(){
+    $('input').prop('disabled', false);
   });
-      var reloadMessages = function() {
-        //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-        last_message_id = $('.message:last').data("message-id");
-        console.log(last_message_id)
-        $.ajax({
-          //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-          url: "api/messages",
-          //ルーティングで設定した通りhttpメソッドをgetに指定
-          type: 'get',
-          dataType: 'json',
-          //dataオプションでリクエストに値を含める
-          data: {id: last_message_id}
-        })
-        .done(function(messages) {
-          if (messages.length !== 0) {
-            //追加するHTMLの入れ物を作る
-            var insertHTML = '';
-            //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-            $.each(messages, function(i, message) {
-              insertHTML += buildHTML(message)
-            });
-            //メッセージが入ったHTMLに、入れ物ごと追加
-            $('.messages').append(insertHTML);
-            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-            $("#new_message")[0].reset();
-            $(".form__submit").prop("disabled", false);
-          }
-        })
-        .fail(function() {
-          console.log('error');
-        });
-      };
+});
+
+var reloadMessages = function() {
+  last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id)
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+    })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $("#new_message")[0].reset();
+      $(".form__submit").prop("disabled", false);
+      }
+    })
+  .fail(function() {
+    alert('error');
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+      setInterval(reloadMessages, 7000);
+      }
     });
+  };
+});
   
